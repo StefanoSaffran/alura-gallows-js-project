fe0429179e525f9dc8cde3a8bf6e2e75eba7db72
Stefano Akira Saffran <stefanoas@gmail.com>
2019-05-06 17:35:51 +0200

    first commit

---
+++ js/sprite.js
@@ -0,0 +1,57 @@
+/* $(() => {
+    var sprite = createSprite('.sprite');
+
+    setInterval(function () {
+        sprite.nextFrame();
+    }, 500);
+}) */
+
+var createSprite = function (selector) {
+
+    var hasNext = function () {
+
+        var next = currentFrame + 1;
+        return next <= last;
+    };
+
+    var moveFrame = function (from, to) {
+
+        $element.removeClass(from)
+            .addClass(to);
+    };
+
+    var nextFrame = function () {
+
+        if (hasNext()) moveFrame(frames[currentFrame], frames[++currentFrame]);
+    };
+
+    var reset = () => {
+
+        moveFrame(frames[current], frames[0])
+        currentFrame = 0;
+    };
+
+    var isFinished = () => {
+
+        return !hasNext();
+    }
+
+    var currentFrame = 0;
+    var $element = $(selector);
+
+    var frames = [
+        'frame1', 'frame2', 'frame3', 'frame4', 'frame5',
+        'frame6', 'frame7', 'frame8', 'frame9'
+    ];
+
+    $element.addClass(frames[currentFrame]);
+    var last = frames.length - 1;
+
+
+    return {
+        nextFrame: nextFrame,
+        reset: reset,
+        isFinished: isFinished
+    };
+
+};
